ff64427cbab3c0d7a6e448a5f05f7d3ade69a06b
Will Cheng <willchengsters@gmail.com>
2018-04-09 12:38:22 -0700

    skeleton of middleware

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,9 @@
 import {combineReducers} from 'redux';
 import listReducer from './list_reducer'
 import {reducer as formReducer} from 'redux-form'
+// import {createStore, applyMiddleware} from 'redux';
+// import promise from 'redux-promise';
+
 export default combineReducers({
     list: listReducer,
     form: formReducer
