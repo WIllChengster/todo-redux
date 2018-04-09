92a00787f5b12e9aae5e0bd6efcec9a23a167e9f
Will Cheng <willchengsters@gmail.com>
2018-04-09 12:23:52 -0700

    completed singleView

--- src/reducers/list_reducer.js
+++ src/reducers/list_reducer.js
@@ -2,12 +2,16 @@ import types from '../actions/types'
 
 const DEFAULT_STATE = {
     items:[],
+    singleItem:{}
 };
 
 export default (state = DEFAULT_STATE, action)=>{
     switch(action.type){
         case types.GET_LIST_DATA:
-            return {...state, items: action.payload.data.todos}
+            return {...state, items: action.payload.data.todos};
+        case types.GET_SINGLE_ITEM:
+            console.log('actions:', action)
+            return {...state, singleItem: action.payload.data.todo}
         default:
             return state;
             
